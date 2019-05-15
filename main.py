d397e8399abf9707cd5988cb47510cf299233fb4
zahrabashir98 <zahra_9877@yahoo.com>
2019-05-15 14:14:49 +0430

    Add main.y which uses both som and processing

---
+++ main.py
@@ -0,0 +1,7 @@
+from processing import process_color_maps
+
+
+img_link = 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Colouring_pencils.jpg'
+n_colors = 40
+
+process_color_maps(n_colors, img_link)
